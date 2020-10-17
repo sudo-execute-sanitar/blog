5acddc9e2a166275265482715adad406d5bb414b
= <=>
2020-10-17 19:21:46 +0300

    Main Page

---
+++ README.md
@@ -0,0 +1,1 @@
+#web_blog \n Please, read requirements.txt file
---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+import flask
+
+
+class Note:
+    def __init__(self,
+                 author="Alex_Designer",
+                 title="Shark Sighting",
+                 text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem adipisci, distinctio qui "
+                      "doloribus neque debitis. Vero accusamus dolorum rerum sed asperiores beatae earum vel. Maiores "
+                      "eveniet iure delectus cum officiis!",
+                 time=datetime.datetime.now(),
+                 image_address="../static/images/shark.jpg"):
+        self.author = author
+        self.title = title
+        self.text = text
+        self.time = time
+        self.image_address = image_address
+
+
+records = [Note()]*2
+
+app = flask.Flask(__name__)
+
+
+@app.route('/notes')
+def notes():
+    return flask.render_template('notes.html', notes=records)
